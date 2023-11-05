00de2e3513c8ee38a31d0f37ffabd5f4b49ac3f5
Santosh Mane <premmane3092001@gmail.com>
2023-11-05 22:27:11 +0530

    Add file and route for testing

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const port = 5000;
+const port = 8080;
 
 //Middleware
 app.use(cors());
@@ -12,8 +13,9 @@ app.use(express.json());
 //Available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
 
 app.listen(port, () => {
   connectToMongo();
-  console.log(`Example app listening on port https://localhost:${port}`);
+  console.log(`App listening on port https://localhost:${port}`);
 });
